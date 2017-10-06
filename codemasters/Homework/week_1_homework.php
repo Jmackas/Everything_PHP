a9f36a855ad5245beb3e4cf7968ecfc43f30f1c3
Jmackas <jamesrossmackay@gmail.com>
2017-10-06 17:37:05 +0800

    Monthly income addition

--- codemasters/Homework/week_1_homework.php
+++ codemasters/Homework/week_1_homework.php
@@ -24,6 +24,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $years = $_POST['loanTerm']; 
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $weeklyIncome = $_POST['weeklyIncome']; 
+}
+
 
 $rate = $interest / 1200;
 $term = $years * 12;
@@ -31,6 +35,9 @@ $term = $years * 12;
 $power = (1 + $rate) ** $term;
 
 $repayments = $loan * $rate * $power / ($power - 1);
+
+/* Weekly income to monthly income */
+$monthlyIncome = $weeklyIncome * 4;
 ?>
 
 <!doctype html>
@@ -59,10 +66,11 @@ $repayments = $loan * $rate * $power / ($power - 1);
 	
 	<p>Please enter the details of your loan below.</p>
 
-	<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 	<p>Loan amount: <input type="text" name="loanAmount"></p>
 	<p>Interest rate: <input type="text" name="interestRate"></p>
 	<p>Loan term: <input type="text" name="loanTerm"></p>
+	<p>Weekly income: <input type="text" name="weeklyIncome"></p>
 	<input type="submit">
   </form>
 
@@ -74,6 +82,8 @@ $repayments = $loan * $rate * $power / ($power - 1);
 	<p>Interest rate: <span class="outputField"><?php echo $interest;?>%</span></p>
 	
 	<p>Loan term: <span class="outputField"><?php echo $years;?> years</span></p>
+
+	<p>Monthly income: <span class="outputField">$<?php echo $monthlyIncome;?></span></p>
 
 	<p><strong>Final repayment: <span class="outputField">$<?php echo number_format($repayments, 2);?> / month</span></strong></p>
 
