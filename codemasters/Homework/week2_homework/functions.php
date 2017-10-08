0cf53ec8d6ce5e4962747827615a5b2eb08f8f64
Jmackas <jamesrossmackay@gmail.com>
2017-10-08 14:04:32 +0800

    Update

--- codemasters/Homework/week2_homework/functions.php
+++ codemasters/Homework/week2_homework/functions.php
@@ -30,4 +30,52 @@
         }
     }
 
+
+
+    /* Function to retrieve the images for landscape */
+    function landscape(){
+        $dir = "photos/landscape/";
+        echo "<h1>Landscape</h1>";
+        // Open a directory, and output contents
+        if (is_dir($dir)){
+            if ($dhe = opendir($dir)){
+                while (($file = readdir($dhe)) !== false){
+                echo "<img src='photos/landscape/$file'><br>";
+                }
+                closedir($dhe);
+            }
+        }
+    }
+
+
+
+        /* Function to retrieve the images for onboard */
+        function onboard(){
+            $dir = "photos/onboard/";
+            echo "<h1>Onboard</h1>";
+            // Open a directory, and output contents
+            if (is_dir($dir)){
+                if ($dhe = opendir($dir)){
+                    while (($file = readdir($dhe)) !== false){
+                    echo "<img src='photos/onboard/$file'><br>";
+                    }
+                    closedir($dhe);
+                }
+            }
+        }
+
+                /* Function to retrieve the images for wildlife */
+                function wildlife(){
+                    $dir = "photos/wildlife/";
+                    echo "<h1>Wildlife</h1>";
+                    // Open a directory, and output contents
+                    if (is_dir($dir)){
+                        if ($dhe = opendir($dir)){
+                            while (($file = readdir($dhe)) !== false){
+                            echo "<img src='photos/wildlife/$file'><br>";
+                            }
+                            closedir($dhe);
+                        }
+                    }
+                }
 ?>
--- codemasters/Homework/week2_homework/index.php
+++ codemasters/Homework/week2_homework/index.php
@@ -1,12 +1,44 @@
 <!-- http://localhost/php_learning/codemasters/Homework/week2_homework/index.php -->
 
+
+
 <?php
 /* Include functions.php in the index.php file */
 include 'functions.php';
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    img {
+        width: 400px;
+        height: 400px;
+    }
+
+</style>
+</head>
+<body>
+
+<?php
 
 echo activityImages();
 echo "<br>";
 echo cuisineImages();
-
+echo "<br>";
+echo landscape();
+echo "<br>";
+echo onboard();
+echo "<br>";
+echo wildlife();
 
 ?>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
