d22c23efe859e4992841c9523630e788639a7735
James <jamesrossmackay@gmail.com>
2017-10-21 10:08:16 +0800

    cms addition

---
+++ codemasters/Week 4/Project/add-home.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Add a new home | Premium Home Building Co.</title>
+	<link rel="stylesheet" href="css/style.css" media="screen" />
+  </head>
+
+  <body>
+    
+    
+    <section class="homes">
+      <div class="container">
+		  <h1>Add a new home</h1>
+		  
+		  <form>
+		  
+		  </form>
+		  
+      </div>
+    </section>
+    
+    
+  </body>
+  
+</html>
