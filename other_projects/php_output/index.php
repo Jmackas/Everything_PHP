18fca3cc4bb347d8d479184de1b4013a7c4dba8c
Jmackas <jamesrossmackay@gmail.com>
2017-11-14 18:13:17 +0800

    update

---
+++ other_projects/php_output/index.php
@@ -0,0 +1,32 @@
+<?php
+
+if (3 < 4) {
+    echo 'meme';
+}
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+pre {
+	display: block;
+	padding: 9.5px;
+	font-size: 13px;
+	line-height: 1.42857143;
+	word-break: break-all;
+	word-wrap: break-word;
+	background-color: #f5f5f5;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+}
+</style>
+</head>
+<body>
+test
+</body>
+</html>
+
