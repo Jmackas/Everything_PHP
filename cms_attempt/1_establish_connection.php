e8b09ab936c49b5a386e31774669f92d46fc8f5d
James <jamesrossmackay@gmail.com>
2017-12-28 09:41:36 +0800

    amalgamated connection establishment and database creation

--- cms_attempt/1_establish_connection.php
+++ cms_attempt/1_establish_connection.php
@@ -16,4 +16,14 @@ echo "Connected successfully";
 
 echo "<br>";
 
+// Create database
+$sql = "CREATE DATABASE my_cms";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+
+echo "<br>";
+
 ?>
