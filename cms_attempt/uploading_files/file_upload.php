a5cf9b2cd029e4d9e686c78c29301536d1c2ab0f
James <jamesrossmackay@gmail.com>
2017-12-28 09:59:40 +0800

    Uploading of files

--- cms_attempt/uploading_files/file_upload.php
+++ cms_attempt/uploading_files/file_upload.php
@@ -15,3 +15,17 @@ if(isset($_POST["submit"])) {
     }
 }
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
